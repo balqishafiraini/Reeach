9e26af4db04180457486d25754e1cb8282bcc98a
James Christian Wira <jamesch888@gmail.com>
2022-10-05 21:36:10 +0700

    Fix: Create folder structure placeholder

---
+++ Reeach/Controller/Controller.swift
@@ -0,0 +1,8 @@
+//
+//  Controller.swift
+//  Reeach
+//
+//  Created by James Christian Wira on 05/10/22.
+//
+
+import Foundation
---
+++ Reeach/Model/Models.swift
@@ -0,0 +1,8 @@
+//
+//  Models.swift
+//  Reeach
+//
+//  Created by James Christian Wira on 05/10/22.
+//
+
+import Foundation
