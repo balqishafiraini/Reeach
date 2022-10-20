895c477c6f718bfc5c0cf92901f4382049acbb6d
James Christian Wira <jamesch888@gmail.com>
2022-10-20 21:42:31 +0700

    Feat: Fetching Inflation Rate from World Bank API

---
+++ Reeach/Helper/Enum/UserDefaultEnum.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultEnum.swift
+//  Reeach
+//
+//  Created by James Christian Wira on 20/10/22.
+//
+
+import Foundation
---
+++ Reeach/Network/EndPoint/InflationEndPoint.swift
@@ -0,0 +1,8 @@
+//
+//  InflationEndPoint.swift
+//  Reeach
+//
+//  Created by James Christian Wira on 13/10/22.
+//
+
+import Foundation
