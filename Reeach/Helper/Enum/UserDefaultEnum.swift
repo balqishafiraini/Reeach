ab589d1587ca030d1e69ef02a89e9da23d871fc7
James Christian Wira <jamesch888@gmail.com>
2022-10-20 21:43:06 +0700

    Feat: Add 'enum' for UserDefaults

--- Reeach/Helper/Enum/UserDefaultEnum.swift
+++ Reeach/Helper/Enum/UserDefaultEnum.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class UserDefaultEnum {
+    public var inflationRate:String = "INFLATION_RATE"
+    public var inflationYear:String = "INFLATION_YEAR"
+    
+    var keys = ["INFLATION_RATE", "INFLATION_YEAR"]
+    
+    func removeAllKey() {
+        print("Deleting key and value for checking")
+        for key in keys {
+            UserDefaults.standard.removeObject(forKey: key)
+        }
+        UserDefaults.standard.synchronize()
+        print("Deleting key done :)")
+    }
+}
