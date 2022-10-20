a291383e3b3da2ab41f67558ee948043d36b36ac
James Christian Wira <jamesch888@gmail.com>
2022-10-20 21:43:53 +0700

    Feat: Add endpoint for Inflation data

--- Reeach/Network/EndPoint/InflationEndPoint.swift
+++ Reeach/Network/EndPoint/InflationEndPoint.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+public enum InflationAPI {
+    case getInflation
+}
+
+extension InflationAPI: EndPointType {
+    var baseUrl: String {
+        return "api.worldbank.org"
+//        return "https://api.worldbank.org"
+//        return "jsonplaceholder.typicode.com"
+    }
+    
+    var path: String {
+        switch self {
+        case .getInflation:
+            return "/v2/country/id/indicator/FP.CPI.TOTL.ZG?format=json"
+//            return "/posts"
+        }
+    }
+    
+    var method: HTTPMethod {
+        switch self {
+        case .getInflation:
+            return .get
+        }
+    }
+    
+    var headers: HTTPHeaders? {
+        return ["Content-Type":"application/json"]
+    }
+    
+}
