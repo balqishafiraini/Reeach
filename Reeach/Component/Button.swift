2dac08db714318049e47fe0569677caeb603f702
Balqis Shafira Aini <balqisshafiraaini@gmail.com>
2022-10-22 12:29:17 +0700

    Feat: Add custom font Custom font extension
    Fix:
    Info plist custom font
    Button font
    TextField font

--- Reeach/Component/Button.swift
+++ Reeach/Component/Button.swift
@@ -15,8 +15,8 @@ class Button: UIButton {
     }
     
     enum Foreground {
-        case black
-        case white
+        case primary
+        case secondary
     }
     
     enum Background {
@@ -79,10 +79,12 @@ class Button: UIButton {
     // background-foreground
     private func handleForegroundButton() {
         switch foreground {
-        case .black:
-            setTitleColor(.black, for: .normal)
-        case .white:
-            setTitleColor(.white, for: .normal)
+        case .primary:
+            UIFont.headline
+            setTitleColor(UIColor.darkSlateGrey, for: .normal)
+        case .secondary:
+            UIFont.headline
+            setTitleColor(UIColor(named: "primary7"), for: .normal)
         }
     }
     
