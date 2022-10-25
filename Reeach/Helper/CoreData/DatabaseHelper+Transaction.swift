cff6292735b3d544d6f22de318fc1675886d3ad7
William Chrisandy <william_chrisandy@yahoo.com>
2022-10-25 10:26:34 +0700

    Feat: Add CoreData entity classes to compile source
    Feat: DatabaseHelper: getStartDateOfMonth
    Feat: DatabaseHelper: getStartDateOfNextMonth
    
    Feat: func getTransactions() -> [Transaction]
    Feat: func getTransaction(id: Int) -> Transaction?
    Feat: func getTransactions(on month: Date) -> [Transaction]
    Feat:
    func getTransactions(of budget: Budget) -> [Transaction]

--- Reeach/Helper/CoreData/DatabaseHelper+Transaction.swift
+++ Reeach/Helper/CoreData/DatabaseHelper+Transaction.swift
@@ -5,4 +5,58 @@
 //  Created by William Chrisandy on 13/10/22.
 //
 
-import Foundation
+import CoreData
+
+extension DatabaseHelper {
+    func getTransactions() -> [Transaction] {
+        do {
+            let fetchRequest: NSFetchRequest<Transaction> = Transaction.fetchRequest()
+            return try context.fetch(fetchRequest)
+        }
+        catch let error {
+            let nsError = error as NSError
+            print("Unresolved error \(nsError), \(nsError.userInfo), \(nsError.localizedDescription)")
+            return []
+        }
+    }
+
+    func getTransaction(id: Int) -> Transaction? {
+        do {
+            let fetchRequest: NSFetchRequest<Transaction> = Transaction.fetchRequest(id: id)
+            let result = try context.fetch(fetchRequest)
+            return result.isEmpty == true ? nil : result[0]
+        }
+        catch let error {
+            let nsError = error as NSError
+            print("Unresolved error \(nsError), \(nsError.userInfo), \(nsError.localizedDescription)")
+            return nil
+        }
+    }
+    
+    func getTransactions(on month: Date) -> [Transaction] {
+        let startDate = DateFormatHelper.getStartDateOfMonth(of: month)
+        let endDate = DateFormatHelper.getStartDateOfNextMonth(of: month)
+        do {
+            let fetchRequest: NSFetchRequest<Transaction> = Transaction.fetchRequest(since: startDate, upTo: endDate)
+            return try context.fetch(fetchRequest)
+        }
+        catch let error {
+            let nsError = error as NSError
+            print("Unresolved error \(nsError), \(nsError.userInfo), \(nsError.localizedDescription)")
+            return []
+        }
+    }
+    
+    func getTransactions(of budget: Budget) -> [Transaction] {
+        do {
+            let fetchRequest: NSFetchRequest<Transaction> = Transaction.fetchRequest(on: budget.period ?? Date(), of: budget.category?.name ?? "")
+            return try context.fetch(fetchRequest)
+        }
+        catch let error {
+            let nsError = error as NSError
+            print("Unresolved error \(nsError), \(nsError.userInfo), \(nsError.localizedDescription)")
+            return []
+        }
+        
+    }
+}
