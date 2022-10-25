45b9a0f7052d193233c7a67500d68a51b4a02bdf
William Chrisandy <william_chrisandy@yahoo.com>
2022-10-26 03:07:36 +0700

    Fix: Delete isAchievable and isActive from Goal in CoreData FIx: Use scalar type for number
    Feat: func getGoals() -> [Goal]
    Feat: func getGoal(name: String) -> Goal?
    Feat: func getGoals(timeTerm: String) -> [Goal]
    Feat: func createGoals(name: String, icon: String, dueDate: Date, targetAmount: Double, timeTerm: String) -> Goal

--- Reeach/Helper/CoreData/DatabaseHelper+Goal.swift
+++ Reeach/Helper/CoreData/DatabaseHelper+Goal.swift
@@ -5,4 +5,61 @@
 //  Created by William Chrisandy on 13/10/22.
 //
 
-import Foundation
+import CoreData
+
+extension DatabaseHelper {
+    func getGoals() -> [Goal] {
+        do {
+            let fetchRequest: NSFetchRequest<Goal> = Goal.fetchRequest()
+            return try context.fetch(fetchRequest)
+        }
+        catch let error {
+            let nsError = error as NSError
+            print("Unresolved error \(nsError), \(nsError.userInfo), \(nsError.localizedDescription)")
+            return []
+        }
+    }
+    
+    func getGoal(name: String) -> Goal? {
+        do {
+            let fetchRequest: NSFetchRequest<Goal> = Goal.fetchRequest(name: name)
+            let result = try context.fetch(fetchRequest)
+            return result.isEmpty == true ? nil : result[0]
+        }
+        catch let error {
+            let nsError = error as NSError
+            print("Unresolved error \(nsError), \(nsError.userInfo), \(nsError.localizedDescription)")
+            return nil
+        }
+    }
+    
+    func getGoals(timeTerm: String) -> [Goal] {
+        do {
+            let fetchRequest: NSFetchRequest<Goal> = Goal.fetchRequest(timeTerm: timeTerm)
+            return try context.fetch(fetchRequest)
+        }
+        catch let error {
+            let nsError = error as NSError
+            print("Unresolved error \(nsError), \(nsError.userInfo), \(nsError.localizedDescription)")
+            return []
+        }
+    }
+    
+    func createGoals(name: String, icon: String, dueDate: Date, targetAmount: Double, timeTerm: String) -> Goal {
+        if let goal = getGoal(name: name) {
+            return goal
+        }
+        else {
+            let goal = Goal(context: context)
+            goal.name = name
+            goal.type = "Goal"
+            goal.icon = icon
+            goal.dueDate = dueDate
+            goal.targetAmount = targetAmount
+            goal.timeTerm = timeTerm
+            goal.createdAt = Date()
+            goal.updatedAt = goal.createdAt
+            return insert(goal)
+        }
+    }
+}
