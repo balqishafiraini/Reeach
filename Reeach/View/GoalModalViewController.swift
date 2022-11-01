0b49e116a54ef595e86f652f9390f7a129ac3364
Balqis Shafira Aini <balqisshafiraaini@gmail.com>
2022-11-01 10:36:28 +0700

    Fix: Layouting

--- Reeach/View/GoalModalViewController.swift
+++ Reeach/View/GoalModalViewController.swift
@@ -38,7 +38,18 @@ class GoalModalViewController: UIViewController {
         
         let icon: UIButton = {
             let button = UIButton()
-            //        let button = Button(style: .circle, foreground: .secondary, background: .royalHunterBlue, title: "Tambah icon")
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.frame.size = CGSize(width: 200, height: 200)
+            button.backgroundColor = UIColor(named: "secondary2")
+            button.setTitle("Tambah Icon", for: .normal)
+            button.setTitleColor(.royalHunterBlue, for: .normal)
+            button.layer.cornerRadius = button.frame.height/2
+            button.layer.masksToBounds = true
+            return button
+        }()
+        
+        let editIcon: UIButton = {
+            let button = UIButton()
             button.translatesAutoresizingMaskIntoConstraints = false
             button.frame.size = CGSize(width: 200, height: 200)
             button.backgroundColor = UIColor(named: "secondary2")
@@ -56,7 +67,7 @@ class GoalModalViewController: UIViewController {
         
         let recommendButton: UIButton = {
             let button = UIButton()
-            button.setTitle("Bingung, guys? Lihat rekomendasinya di sini, yuk!", for: .normal)
+            button.setTitle("Bingung, guys? Lihat rekomendasinya di sini, kuy!", for: .normal)
             button.contentHorizontalAlignment = .left
             button.backgroundColor = .clear
             button.setTitleColor(.royalHunterBlue, for: .normal)
@@ -64,22 +75,14 @@ class GoalModalViewController: UIViewController {
             button.titleLabel?.textAlignment = .left
             return button
         }()
+
         
-        let dueName: UILabel = {
-            let label = UILabel()
-            label.text = "Deadline"
-            label.numberOfLines = 1
-            label.textAlignment = .left
-            label.font = .bodyBold
-            label.translatesAutoresizingMaskIntoConstraints = false
-            return label
-        }()
-        
-        let dueDate = DatePicker(frame: .zero)
+        let dueDate = DatePicker(frame: .zero, title: "Deadline")
         
         let inflationButton: UIButton = {
             let button = UIButton()
-            button.setTitle("WATCH OUT!", for: .normal)
+            button.setImage(UIImage(systemName: "exclamationmark.circle"), for: .normal)
+            button.setTitle("WATCH OUT! Nilai setelah inflasi: ", for: .normal)
             button.contentHorizontalAlignment = .left
             button.backgroundColor = .clear
             button.setTitleColor(.royalHunterBlue, for: .normal)
@@ -96,10 +99,10 @@ class GoalModalViewController: UIViewController {
         hstack.frame = view.bounds
         hstack.axis = .horizontal
         hstack.distribution = .fillEqually
-        hstack.spacing = 30
+        hstack.spacing = 20
         hstack.translatesAutoresizingMaskIntoConstraints = false
         
-        let vstack = UIStackView(arrangedSubviews: [hstack, icon, goalName, recommendButton, dueName, dueDate, total, inflationButton, savingAmount])
+        let vstack = UIStackView(arrangedSubviews: [hstack, icon, goalName, recommendButton, dueDate, total, inflationButton, savingAmount])
         vstack.frame = view.bounds
         vstack.axis = .vertical
         vstack.distribution = .fill
@@ -110,7 +113,7 @@ class GoalModalViewController: UIViewController {
         
         vstack.backgroundColor = .white
         
-        hstack.anchor(top:vstack.topAnchor, left: vstack.leftAnchor, right: vstack.rightAnchor, paddingTop: 12)
+        hstack.anchor(top:view.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 12,paddingLeft: 20, paddingRight: 20)
         
         vstack.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor)
         
@@ -120,8 +123,6 @@ class GoalModalViewController: UIViewController {
         
         recommendButton.anchor(left: vstack.leftAnchor, right: vstack.rightAnchor, paddingLeft: 20, paddingRight: 20)
         
-        dueName.anchor(left: vstack.leftAnchor, right: vstack.rightAnchor, paddingLeft: 20, paddingRight: 20)
-        
         dueDate.anchor(left: vstack.leftAnchor, right: vstack.rightAnchor, paddingLeft: 20, paddingRight: 20)
         
         total.anchor(top: dueDate.topAnchor, left: vstack.leftAnchor, right: vstack.rightAnchor, paddingTop: 80, paddingLeft: 20, paddingRight: 20)
@@ -129,42 +130,12 @@ class GoalModalViewController: UIViewController {
         inflationButton.anchor(left: vstack.leftAnchor, right: vstack.rightAnchor, paddingLeft: 20, paddingRight: 20)
     }
     
-    
-    
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureStackView()
     }
-    
-    //        view.addSubview(cancelButton)
-    //        cancelButton.anchor(top: view.topAnchor, left: view.leftAnchor, paddingTop: 20, paddingLeft: 20)
-    //
-    //        view.addSubview(addGoal)
-    //        addGoal.centerX(inView: view, topAnchor: view.topAnchor, paddingTop: 26)
-    //
-    //        view.addSubview(saveButton)
-    //        saveButton.anchor(top: view.topAnchor, right: view.rightAnchor, paddingTop: 20, paddingRight: 20)
-    //
-    //        view.addSubview(icon)
-    //        icon.centerX(inView: addGoal, topAnchor: addGoal.topAnchor, paddingTop: 100)
-    //        icon.anchor(width: view.frame.size.width/2, height: view.frame.size.width/2)
-    //
-    //        view.addSubview(editIcon)
-    //        editIcon.centerX(inView: addGoal, topAnchor: addGoal.topAnchor, paddingTop: 120)
-    //
-    //        view.addSubview(goalName)
-    //        goalName.anchor(top: icon.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 200, paddingLeft: 20, paddingRight: 20)
-    //
-    //        view.addSubview(dueDate)
-    //        dueDate.anchor(top: goalName.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 200, paddingLeft: 20, paddingRight: 20)
-    //
-    //        view.addSubview(total)
-    //        total.anchor(top: view.topAnchor, left: view.leftAnchor, paddingTop: 280, paddingLeft: 20)
-    //
-    //        view.addSubview(savingAmount)
-    //        savingAmount.anchor(top: view.topAnchor, left: view.leftAnchor, paddingTop: 360, paddingLeft: 20)
 }
 
 
