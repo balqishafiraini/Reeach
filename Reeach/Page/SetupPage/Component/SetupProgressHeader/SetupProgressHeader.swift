8510d2397a423d5096e359ae6fdb7d8ddcc1e860
James Christian Wira <jamesch888@gmail.com>
2022-11-14 04:19:00 +0700

    Feat: Insert steps into SetupProgressHeader
    Additional changes:
    - Change the auto layout code for progress bar to conform steps
    - Create function to update steps view based on currentProgressIndex from SetupPageViewController

---
+++ Reeach/Page/SetupPage/Component/SetupProgressHeader/SetupProgressHeader.swift
@@ -0,0 +1,84 @@
+//
+//  SetupProgressHeader.swift
+//  Reeach
+//
+//  Created by James Christian Wira on 24/10/22.
+//
+
+import UIKit
+
+class SetupProgressHeader: UIView {
+
+    weak var controller: SetupPageViewController?
+    
+    var progressValue: Float = 0.0
+    
+    lazy var progressBar: UIProgressView = {
+        var view = UIProgressView(progressViewStyle: .bar)
+        
+        view.trackTintColor = UIColor(named: "neutral5")
+        view.tintColor = UIColor(named: "secondary6")
+        
+        return view
+    }()
+    
+    lazy var firstStep = ProgressSection(frame: .zero, progressNumber: "1", progressLabel: "Goal-Setting", isActive: true)
+    lazy var secondStep = ProgressSection(frame: .zero, progressNumber: "2", progressLabel: "Pemasukan")
+    lazy var thirdStep = ProgressSection(frame: .zero, progressNumber: "3", progressLabel: "Budget")
+    
+    init(frame: CGRect, controller: SetupPageViewController) {
+        self.controller = controller
+        
+        super.init(frame: frame)
+        
+        setupView()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupView() {
+        self.setDimensions(width: UIScreen.main.bounds.width - 32, height: 50)
+        
+        self.addSubview(progressBar)
+        self.addSubview(firstStep)
+        self.addSubview(secondStep)
+        self.addSubview(thirdStep)
+        
+        progressBar.setProgress(progressValue, animated: true)
+        progressBar.center(inView: self, yConstant: -8)
+        
+        firstStep.anchor(left: self.leftAnchor)
+        secondStep.centerX(inView: progressBar)
+        thirdStep.anchor(right: self.rightAnchor)
+        
+        NSLayoutConstraint.activate([
+            progressBar.leftAnchor.constraint(equalTo: firstStep.numberView.rightAnchor),
+            progressBar.rightAnchor.constraint(equalTo: thirdStep.numberView.leftAnchor)
+        ])
+    }
+    
+    func updateSteps(currentIndex: Float) {
+        // This could be better, I just don't know how to do it now
+        
+        if currentIndex >= 0.0 {
+            firstStep.updateView(isActive: true)
+            secondStep.updateView(isActive: false)
+            thirdStep.updateView(isActive: false)
+        }
+        if currentIndex >= 1.0 {
+            secondStep.updateView(isActive: true)
+            thirdStep.updateView(isActive: false)
+        }
+        if currentIndex >= 2.0 {
+            thirdStep.updateView(isActive: true)
+        }
+    }
+}
