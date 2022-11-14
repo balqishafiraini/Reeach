05404df73c1b92676141e02e59d1ec87760d3c3f
Balqis Shafira Aini <balqisshafiraaini@gmail.com>
2022-11-15 00:05:55 +0700

    Fix: Wants and Needs in 1 file, add navigationBar for title

---
+++ Reeach/Page/SetupPage/Component/CategoryAllocation/Allocation/Controller/GoalAllocationViewController.swift
@@ -0,0 +1,47 @@
+//
+//  GoalAllocationViewController.swift
+//  Reeach
+//
+//  Created by Balqis on 14/11/22.
+//
+
+import UIKit
+
+class GoalAllocationViewController: UIViewController {
+    
+    var type: String = "Needs"
+    
+    let goalAllocationModalView = GoalAllocationModalView()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        goalAllocationModalView.configureStackView()
+        view = goalAllocationModalView
+        title = "Kebutuhan Non-Pokok"
+        self.setNavigationBar()
+    }
+    
+    override func loadView() {
+        super.loadView()
+        title = type == "Needs" ? "Kebutuhan Pokok" : "Kebutuhan Non-Pokok"
+    }
+    
+    func setNavigationBar() {
+        let doneItem = UIBarButtonItem(title: "Batal", style: .plain, target: self, action: #selector(dismissView))
+        
+        let attributes: [NSAttributedString.Key : Any] = [
+            NSAttributedString.Key.foregroundColor: UIColor.red6 as Any,
+            NSAttributedString.Key.font: UIFont.bodyMedium as Any
+        ]
+        
+        doneItem.setTitleTextAttributes(attributes, for: .normal)
+        
+        navigationItem.leftBarButtonItem = doneItem
+    }
+    
+    @objc func dismissView(){
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
--- Reeach/Page/SetupPage/Component/CategoryAllocation/Allocation/View/GoalAllocationModalView.swift
+++ Reeach/Page/SetupPage/Component/CategoryAllocation/Allocation/View/GoalAllocationModalView.swift
@@ -1,5 +1,5 @@
 //
-//  BudgetNeedsModalView.swift
+//  GoalAllocationView.swift
 //  Reeach
 //
 //  Created by Balqis on 14/11/22.
@@ -7,27 +7,8 @@
 
 import UIKit
 
-class BudgetNeedsModalView: UIView {
-    
-    let cancelButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Batal", for: .normal)
-        button.setTitleColor(UIColor.crimson, for: .normal)
-        button.backgroundColor = .clear
-        button.contentHorizontalAlignment = .left
-        return button
-    }()
-    
-    let goalAllocationTitle: UILabel = {
-        let label = UILabel()
-        label.text = "Kebutuhan Pokok"
-        label.textAlignment = .center
-        label.font = .bodyBold
-        label.setContentHuggingPriority(UILayoutPriority(251), for: .vertical)
-        return label
-    }()
-    
-    
+class GoalAllocationModalView: UIView {
+        
     let iconWithoutEdit = IconWithoutEditView()
     
     let category = TextField(frame: .zero, title: "Kategori", style: .template, icon: UIImage(systemName: "greaterthan"))
@@ -36,8 +17,6 @@ class BudgetNeedsModalView: UIView {
     
     let saveButton = Button(style: .rounded, foreground: .primary, background: .tangerineYellow, title: "Simpan")
 
-    
-    //scrollView
     lazy var scrollView: UIScrollView = {
             let scrollView = UIScrollView()
             return scrollView
@@ -46,11 +25,6 @@ class BudgetNeedsModalView: UIView {
     
     func configureStackView() {
         self.backgroundColor = .ghostWhite
-        
-        let navHstack = UIStackView(arrangedSubviews: [cancelButton, goalAllocationTitle])
-        navHstack.frame = self.bounds
-        navHstack.axis = .horizontal
-        navHstack.distribution = .fill
         
         let vstack = UIStackView(arrangedSubviews: [iconWithoutEdit,
                                                     category,
@@ -62,17 +36,13 @@ class BudgetNeedsModalView: UIView {
         vstack.axis = .vertical
         vstack.distribution = .fill
         vstack.spacing = 16
-
+        
         
         //autolayout
+        self.addSubview(scrollView)
         scrollView.addSubview(vstack)
         
-        self.addSubview(navHstack)
-        self.addSubview(scrollView)
-        
-        navHstack.anchor(top: self.topAnchor, left: self.leftAnchor, right: self.rightAnchor, paddingLeft: 20, paddingRight: 20, height: 50)
-        
-        scrollView.anchor(top: navHstack.bottomAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, width: UIScreen.main.bounds.width)
+        scrollView.anchor(top: safeAreaLayoutGuide.topAnchor, left: safeAreaLayoutGuide.leftAnchor, bottom: safeAreaLayoutGuide.bottomAnchor, right: safeAreaLayoutGuide.rightAnchor, width: UIScreen.main.bounds.width)
         
         vstack.anchor(top: scrollView.topAnchor, left: scrollView.leftAnchor, bottom: scrollView.bottomAnchor, right: scrollView.rightAnchor, paddingLeft: 20, paddingRight: 20)
         
@@ -81,11 +51,11 @@ class BudgetNeedsModalView: UIView {
         ])
         
         iconWithoutEdit.setUp()
+        iconWithoutEdit.anchor(top: vstack.topAnchor, paddingTop: 20)
         iconWithoutEdit.heightAnchor.constraint(equalTo: iconWithoutEdit.iconLabel.heightAnchor).isActive = true
         
-        goalAllocationTitle.anchor(right: vstack.rightAnchor, paddingRight: UIScreen.main.bounds.width*0.25)
-        
     }
+
 }
 
 class IconWithoutEditView: UIView {
@@ -112,5 +82,4 @@ class IconWithoutEditView: UIView {
         
         iconLabel.anchor(width: 150, height: 150)
     }
-
 }
