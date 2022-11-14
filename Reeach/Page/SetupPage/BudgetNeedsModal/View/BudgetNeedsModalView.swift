0305060b10b4324dd8d283769e03da7b9deadf50
Balqis Shafira Aini <balqisshafiraaini@gmail.com>
2022-11-14 19:21:44 +0700

    Feat: Folder structuring for budgetign wants and needs modal

---
+++ Reeach/Page/SetupPage/BudgetNeedsModal/View/BudgetNeedsModalView.swift
@@ -0,0 +1,8 @@
+//
+//  BudgetNeedsModalView.swift
+//  Reeach
+//
+//  Created by Balqis on 14/11/22.
+//
+
+import Foundation
---
+++ Reeach/Page/SetupPage/BudgetsWantsModal/View/BudgetWantsModalView.swift
@@ -0,0 +1,8 @@
+//
+//  BudgetWantsModal.swift
+//  Reeach
+//
+//  Created by Balqis on 14/11/22.
+//
+
+import Foundation
