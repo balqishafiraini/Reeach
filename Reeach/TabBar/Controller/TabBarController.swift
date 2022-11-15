07e50ed6e96ddd7581daa19cfc91826d45847529
James Christian Wira <jamesch888@gmail.com>
2022-11-15 09:23:20 +0700

    Feat: Fetch inflation data from API

--- Reeach/TabBar/Controller/TabBarController.swift
+++ Reeach/TabBar/Controller/TabBarController.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class TabBarController: UITabBarController {
+    
+    let inflationEndPoint = InflationAPI.getInflation
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,6 +56,52 @@ class TabBarController: UITabBarController {
         tabBar.frame.origin.y = view.frame.height - 85
     }
 }
+
+extension TabBarController: NetworkDelegate {
+    func onResponse(from endPoint: EndPointType?, result: Result<Data, NetworkError>) {
+        if let _ = endPoint as? InflationAPI {
+            switch result {
+            case .success(let data):
+                do {
+                    if let json = try? JSONSerialization.jsonObject(with: data) as? [Any] {
+                        let decoder = JSONDecoder()
+                        let inflationNetwork: [InflationDetail] = try decoder.decode([InflationDetail].self, from: JSONSerialization.data(withJSONObject: json[1], options: .fragmentsAllowed))
+                        
+                        saveInflationToUserDefaults(inflationNetwork[0])
+                    }
+                } catch {
+                    print(error)
+                }
+                
+            case .failure(let error):
+                print(error.rawValue)
+            }
+        }
+    }
+    
+    func saveInflationToUserDefaults(_ inflationData: InflationDetail) {
+        let defaults = UserDefaults.standard
+        defaults.set(inflationData.date, forKey: UserDefaultEnum().inflationYear)
+        defaults.set(inflationData.value, forKey: UserDefaultEnum().inflationRate)
+    }
+}
+
+extension TabBarController {
+    func shouldGetNewRate() -> Bool {
+        return UserDefaults.standard.object(forKey: UserDefaultEnum().inflationRate) == nil && Int(UserDefaults.standard.string(forKey: UserDefaultEnum().inflationYear) ?? "2000") != Calendar.current.component(.year, from: Date())
+    }
+    
+    func setupInflationData() {
+//        if shouldGetNewRate() {
+        print("Get current inflation rate")
+        let networkManager = NetworkManager()
+        networkManager.networkDelegate = self
+        networkManager.createRequest(with: inflationEndPoint)
+        return
+//        }
+    }
+}
+
 
 
 class GoalsVC: UIViewController {
