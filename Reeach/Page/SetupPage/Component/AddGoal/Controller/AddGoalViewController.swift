ad418187c4ad991aba6b36a1bbc82f7992efb679
James Christian Wira <jamesch888@gmail.com>
2022-11-16 10:51:32 +0700

    Fix: Change AddGoal to View and ViewController
    Additional changes:
    - Change add button to round button
    - Add Logic if view has no goal an has/have goal(s)

---
+++ Reeach/Page/SetupPage/Component/AddGoal/Controller/AddGoalViewController.swift
@@ -0,0 +1,71 @@
+//
+//  AddGoalViewController.swift
+//  Reeach
+//
+//  Created by James Christian Wira on 15/11/22.
+//
+
+import UIKit
+
+class AddGoalViewController: UIViewController {
+
+    weak var delegate: SetupDelegate?
+    
+    var addGoal: AddGoal = AddGoal(frame: .zero)
+    
+    var goals: [Goal] = []
+    var isFirstTime: Bool = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addGoal.delegate = delegate
+        addGoal.controller = self
+        
+        self.view = addGoal
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        goals = DatabaseHelper().getGoals()
+        
+        addGoal.goals = goals
+        addGoal.setupView()
+        addGoal.goalList.reloadData()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        isFirstTime = false
+    }
+}
+
+extension AddGoalViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("Collection view at row \(collectionView.tag) selected index path \(indexPath)")
+    }
+}
+
+extension AddGoalViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        goals.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: GoalItemCollectionViewCell.identifier, for: indexPath) as! GoalItemCollectionViewCell
+        
+        cell.title = goals[indexPath.row].name
+        cell.dueDate = goals[indexPath.row].dueDate
+        cell.iconName = goals[indexPath.row].icon
+        cell.amount = goals[indexPath.row].targetAmount
+        
+        cell.configureData()
+        
+        cell.setNeedsLayout()
+
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize
+    {
+        return CGSize(width: collectionView.frame.width, height: 104)
+    }
+}
