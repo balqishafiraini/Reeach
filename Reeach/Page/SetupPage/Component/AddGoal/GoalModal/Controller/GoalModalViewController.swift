bc36c57808477cd60d31d2b84f22cefa9c6a41a5
Balqis Shafira Aini <balqisshafiraaini@gmail.com>
2022-11-16 21:51:10 +0700

    Fix: Add delegate, numberpad, iconview input,

--- Reeach/Page/SetupPage/Component/AddGoal/GoalModal/Controller/GoalModalViewController.swift
+++ Reeach/Page/SetupPage/Component/AddGoal/GoalModal/Controller/GoalModalViewController.swift
@@ -29,11 +29,22 @@ class GoalModalViewController: UIViewController {
         goalModalView.configureStackView()
         view = goalModalView
         
+        goalModalView.goalType.textField.delegate = self
+        goalModalView.total.textField.delegate = self
+        goalModalView.switchView.tf.textField.delegate = self
+        goalModalView.iconView.iconTextField.delegate = self
+        
+        goalModalView.total.textField.keyboardType = .numberPad
+        goalModalView.switchView.tf.textField.keyboardType = .numberPad
+            
         goalModalView.recommendButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         
         goalModalView.inflationButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         
         goalModalView.saveButton.addTarget(self, action: #selector(saveGoalToCoreData), for: .touchUpInside)
+        
+        //add target edit button
+        goalModalView.iconView.editButton.addTarget(self, action: #selector(editIconTapped), for: .touchUpInside)
         
         self.hideKeyboardWhenTappedAround()
         
@@ -48,30 +59,35 @@ class GoalModalViewController: UIViewController {
         let amount = Double(goalModalView.total.textField.text ?? "0.0")
         let initSaving = Double(goalModalView.switchView.tf.textField.text ?? "0.0")
         
-        self.saveInput(iconName: goalModalView.iconView.iconLabel.text ?? "iphone", goal: goalModalView.goalName.textField.text, deadline: goalModalView.dueDate.date, amount: amount, goalType: goalModalView.goalType.textField.text, initSaving: initSaving)
+        self.saveInput(iconName: goalModalView.iconView.iconTextField.text ?? "iphone", goal: goalModalView.goalName.textField.text, deadline: goalModalView.dueDate.date, amount: amount, goalType: goalModalView.goalType.textField.text, initSaving: initSaving)
     }
     
     @objc func buttonTapped(_ sender: UIButton!) {
         print(#function)
     }
     
+    @objc func editIconTapped(_ sender: UIButton!) {
+        goalModalView.iconView.iconTextField.isUserInteractionEnabled = true
+        goalModalView.iconView.iconTextField.becomeFirstResponder()
+    }
+    
     func setNavigationBar() {
-       let doneItem = UIBarButtonItem(title: "Batal", style: .plain, target: self, action: #selector(dismissView))
-
-       let attributes: [NSAttributedString.Key : Any] = [
-           NSAttributedString.Key.foregroundColor: UIColor.red6 as Any,
-           NSAttributedString.Key.font: UIFont.bodyMedium as Any
-       ]
-
-       doneItem.setTitleTextAttributes(attributes, for: .normal)
-
-       navigationItem.leftBarButtonItem = doneItem
-   }
-
-   @objc func dismissView(){
-       self.dismiss(animated: true, completion: nil)
-   }
-
+        let doneItem = UIBarButtonItem(title: "Batal", style: .plain, target: self, action: #selector(dismissView))
+        
+        let attributes: [NSAttributedString.Key : Any] = [
+            NSAttributedString.Key.foregroundColor: UIColor.red6 as Any,
+            NSAttributedString.Key.font: UIFont.bodyMedium as Any
+        ]
+        
+        doneItem.setTitleTextAttributes(attributes, for: .normal)
+        
+        navigationItem.leftBarButtonItem = doneItem
+    }
+    
+    @objc func dismissView(){
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 }
 
 extension GoalModalViewController: GoalSetupDelegate {
@@ -87,3 +103,39 @@ extension GoalModalViewController: GoalSetupDelegate {
     }
 }
 
+extension GoalModalViewController: UITextFieldDelegate {
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        if textField == goalModalView.goalType.textField {
+            return false
+        }
+        
+        if textField == goalModalView.iconView.iconTextField {
+            let maxLength = 1
+            let currentString = (textField.text ?? "") as NSString
+            let newString = currentString.replacingCharacters(in: range, with: string)
+
+            return newString.count <= maxLength
+        }
+        
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        
+        if textField == goalModalView.goalType.textField {
+            let goalVC = TermPickerViewController()
+            navigationController?.pushViewController(goalVC, animated: true)
+        } else {
+            goalModalView.scrollView.setContentOffset(CGPoint.init(x: 0, y: UIScreen.main.bounds.height/3), animated: true)
+        }
+    }
+    
+    private func textFieldDidEndEditing(textField: UITextField!) {
+        goalModalView.scrollView.setContentOffset(CGPoint.init(x: 0, y: 0), animated: true)
+
+        self.view.endEditing(true);
+    }
+    
+    
+}
--- Reeach/Page/SetupPage/Component/AddGoal/GoalModal/View/GoalModalView.swift
+++ Reeach/Page/SetupPage/Component/AddGoal/GoalModal/View/GoalModalView.swift
@@ -24,7 +24,7 @@ class GoalModalView: UIView {
         button.titleLabel?.textAlignment = .left
         button.layer.cornerRadius = 16
         button.contentEdgeInsets = UIEdgeInsets(top: 12, left: 12, bottom: 12, right: 12)
-
+        
         button.setContentHuggingPriority(UILayoutPriority(251), for: .vertical)
         return button
     }()
@@ -48,7 +48,11 @@ class GoalModalView: UIView {
     
     let total = TextField(frame: .zero, title: "Jumlah", style: .template, prefix: "Rp")
     
-    let goalType = TextField(frame: .zero, title: "Tipe Goal", style: .template, icon: UIImage(systemName: "greaterthan"))
+    let goalType = {
+        let tf = TextField(frame: .zero, title: "Tipe Goal", style: .template, icon: UIImage(systemName: "greaterthan"))
+        tf.tintColor = .clear
+        return tf
+    }()
     
     let iconView = IconView()
     
@@ -56,11 +60,10 @@ class GoalModalView: UIView {
     
     let saveButton = Button(style: .rounded, foreground: .primary, background: .tangerineYellow, title: "Simpan")
     
-    //scrollView
     lazy var scrollView: UIScrollView = {
-            let scrollView = UIScrollView()
-            return scrollView
-        }()
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
     
     func configureStackView() {
         self.backgroundColor = .ghostWhite
@@ -73,7 +76,7 @@ class GoalModalView: UIView {
                                                     inflationButton,
                                                     goalType,
                                                     switchView,
-                                                   saveButton]
+                                                    saveButton]
         )
         
         vstack.frame = self.bounds
@@ -85,132 +88,108 @@ class GoalModalView: UIView {
         
         //autolayout
         self.addSubview(scrollView)
-
+        
         scrollView.addSubview(vstack)
         
         scrollView.anchor(top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, width: UIScreen.main.bounds.width)
-
+        
         vstack.anchor(top: scrollView.topAnchor, left: scrollView.leftAnchor, bottom: scrollView.bottomAnchor, right: scrollView.rightAnchor, paddingLeft: 20, paddingRight: 20)
-
+        
         NSLayoutConstraint.activate([
             vstack.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -40)
         ])
         
-        
-        //iconview
         iconView.setUp()
-        iconView.heightAnchor.constraint(equalTo: iconView.iconLabel.heightAnchor).isActive = true
+        iconView.heightAnchor.constraint(equalTo: iconView.iconTextField.heightAnchor).isActive = true
         
-        //switchview
         switchView.setupView()
     }
     
-//    func setupTriggerForm() {
-//        goalName.textField.addTarget(self, action: #selector(textFieldTapped), for: .allEvents)
-//        goalName.textField.sendActions(for: .valueChanged)
-//
-////        dueDate.textField.addTarget(self, action: #selector(textFieldTapped), for: .allEvents)
-////        dueDate.textField.sendActions(for: .valueChanged)
-//
-//        total.textField.addTarget(self, action: #selector(textFieldTapped), for: .allEvents)
-//        total.textField.sendActions(for: .valueChanged)
-//
-////        goalType.textField.addTarget(self, action: #selector(textFieldTapped), for: .allEvents)
-////        goalType.textField.sendActions(for: .valueChanged)
-//
-//        switchView.tf.textField.addTarget(self, action: #selector(textFieldTapped), for: .allEvents)
-//        switchView.tf.textField.sendActions(for: .valueChanged)
-//    }
-//
-//    @objc func textFieldTapped(_ sender: UITextField!) {
-//        print("TextField clicked")
-//    }
-}
-
-class IconView: UIView {
-    let iconLabel: UILabel = {
+    class IconView: UIView {
+        let iconTextField: UIEmojiTextField = {
+            let tf = UIEmojiTextField()
+            tf.font = UIFont.systemFont(ofSize: 80)
+            tf.attributedPlaceholder = NSAttributedString(string:"Tambah Icon", attributes:[NSAttributedString.Key.font :UIFont.bodyMedium!, NSAttributedString.Key.foregroundColor:UIColor.royalHunterBlue!])
+            tf.textAlignment = .center
+            tf.tintColor = .clear
+            tf.backgroundColor = .secondary2
+            tf.layer.cornerRadius = 75
+            tf.layer.masksToBounds = true
+            tf.isUserInteractionEnabled = false
+            return tf
+        }()
         
-        let label = UILabel()
-        label.text = "Tambah icon"
-        label.textAlignment = .center
-        label.textColor = .royalHunterBlue
-        label.font = .bodyMedium
-        label.backgroundColor = .secondary2
-        label.layer.cornerRadius = 75
-        label.layer.masksToBounds = true
-        return label
-    }()
-    
-    let editButton: UIButton = {
+        let editButton: UIButton = {
+            let button = UIButton()
+            button.backgroundColor = .tangerineYellow
+            button.tintColor = .royalHunterBlue
+            button.contentEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+            button.layer.cornerRadius = 25
+            let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .black)
+            let symbol = UIImage(systemName: "pencil", withConfiguration: config)
+            button.setImage(symbol, for: .normal)
+            return button
+        }()
         
-        let button = UIButton()
-        button.backgroundColor = .tangerineYellow
-        button.contentEdgeInsets = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
-        button.layer.cornerRadius = 25
-        
-        let config = UIImage.SymbolConfiguration(pointSize: 25, weight: .black)
-        let symbol = UIImage(systemName: "pencil", withConfiguration: config)
-        button.setImage(symbol, for: .normal)
-        return button
-    }()
-    
-    func setUp() {
-        //auto-layout
-        
-        addSubview(editButton)
-        addSubview(iconLabel)
-        bringSubviewToFront(editButton)
-        
-        editButton.anchor(bottom: iconLabel.bottomAnchor, right: iconLabel.rightAnchor, paddingRight: 8)
-        
-        iconLabel.centerX(inView: self)
-        
-        iconLabel.anchor(width: 150, height: 150)
-    }
-
-}
-
-class SwitchView: UIView {
-    
-    let didHaveInitSaving: UILabel = {
-        let label = UILabel()
-        label.text = "Punya tabungan awal?"
-        label.font = .bodyBold
-        label.setContentHuggingPriority(UILayoutPriority(251), for: .vertical)
-        return label
-    }()
-    
-    var toggleSwitch: UISwitch = {
-        let toggle = UISwitch()
-        toggle.setOn(false, animated: true)
-        toggle.onTintColor = .royalHunterBlue
-        return toggle
-    }()
-    
-    let tf = TextField(frame: .zero, style: .template, prefix: "Rp")
-    
-    @objc func switchStateDidChange(_ sender:UISwitch!) {
-        if (toggleSwitch.isOn == true){
-//            makePrefix()
-            tf.isHidden = false
+        func setUp() {
+            //auto-layout
+            
+            addSubview(editButton)
+            addSubview(iconTextField)
+            bringSubviewToFront(editButton)
+            
+            editButton.anchor(bottom: iconTextField.bottomAnchor, right: iconTextField.rightAnchor, paddingRight: 8)
+            
+            iconTextField.centerX(inView: self)
+            
+            iconTextField.anchor(width: 150, height: 150)
         }
-        else{
-//            makePrefix()
+        
+    }
+    
+    class SwitchView: UIView {
+        
+        let initSavingLabel: UILabel = {
+            let label = UILabel()
+            label.text = "Punya tabungan awal?"
+            label.font = .bodyBold
+            label.setContentHuggingPriority(UILayoutPriority(251), for: .vertical)
+            return label
+        }()
+        
+        var toggleSwitch: UISwitch = {
+            let toggle = UISwitch()
+            toggle.setOn(false, animated: true)
+            toggle.onTintColor = .royalHunterBlue
+            return toggle
+        }()
+        
+        let tf = TextField(frame: .zero, style: .template, prefix: "Rp")
+        
+        @objc func switchStateDidChange(_ sender:UISwitch!) {
+            if (toggleSwitch.isOn == true){
+                //            makePrefix()
+                tf.isHidden = false
+            }
+            else{
+                //            makePrefix()
+                tf.isHidden = true
+            }
+        }
+        
+        func setupView() {
+            addSubview(initSavingLabel)
+            initSavingLabel.anchor(top: self.topAnchor, left: self.leftAnchor)
+            
+            addSubview(toggleSwitch)
+            toggleSwitch.anchor(top: self.topAnchor, left: initSavingLabel.rightAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, paddingLeft: 50)
+            toggleSwitch.addTarget(self, action: #selector(switchStateDidChange), for: .valueChanged)
+            
+            addSubview(tf)
+            tf.anchor(top: initSavingLabel.topAnchor, left: initSavingLabel.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, paddingTop: 40, paddingBottom: 20)
             tf.isHidden = true
         }
-    }
-    
-    func setupView() {
-        addSubview(didHaveInitSaving)
-        didHaveInitSaving.anchor(top: self.topAnchor, left: self.leftAnchor)
         
-        addSubview(toggleSwitch)
-        toggleSwitch.anchor(top: self.topAnchor, left: didHaveInitSaving.rightAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, paddingLeft: 50)
-        toggleSwitch.addTarget(self, action: #selector(switchStateDidChange), for: .valueChanged)
-        
-        addSubview(tf)
-        tf.anchor(top: didHaveInitSaving.topAnchor, left: didHaveInitSaving.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, paddingTop: 40, paddingBottom: 20)
-        tf.isHidden = true
     }
     
 }
