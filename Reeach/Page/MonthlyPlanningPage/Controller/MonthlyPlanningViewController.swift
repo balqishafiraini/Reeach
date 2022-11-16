081242ef1aadbd60e6093b650b252c7d2caf3805
James Christian Wira <jamesch888@gmail.com>
2022-11-17 03:29:35 +0700

    Fix: initiate date data for view

--- Reeach/Page/MonthlyPlanningPage/Controller/MonthlyPlanningViewController.swift
+++ Reeach/Page/MonthlyPlanningPage/Controller/MonthlyPlanningViewController.swift
@@ -11,12 +11,19 @@ class MonthlyPlanningViewController: UIViewController {
 
     let planningView = MonthlyPlanningView()
     
+    var currentDate: Date = Date()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func loadView() {
         self.view = planningView
+        planningView.delegate = self
+        planningView.setupDate(currentDate: currentDate)
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        getPlanner(forDate: DateFormatHelper.getStartDateOfMonth(of: Date()))
+    }
 }
