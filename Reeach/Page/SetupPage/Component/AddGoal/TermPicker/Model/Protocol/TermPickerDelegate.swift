c2a8463b283a2def1010fade5fe0090ced056e16
William Chrisandy <william_chrisandy@yahoo.com>
2022-11-16 08:29:57 +0700

    Feat: TermPickerViewController: delegate, didSelectItemAt Feat: TermPickerViewControllerDelegate: selected

---
+++ Reeach/Page/SetupPage/Component/AddGoal/TermPicker/Model/Protocol/TermPickerDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  TermPickerDelegate.swift
+//  Reeach
+//
+//  Created by William Chrisandy on 16/11/22.
+//
+
+import Foundation
+
+protocol TermPickerViewControllerDelegate: AnyObject {
+    func selected(timeTerm: String)
+}
