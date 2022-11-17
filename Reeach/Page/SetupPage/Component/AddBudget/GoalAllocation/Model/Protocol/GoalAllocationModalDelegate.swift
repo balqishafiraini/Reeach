2d26d07c56a435727c9298d37c2731ecad1f6aa5
Balqis Shafira Aini <balqisshafiraaini@gmail.com>
2022-11-17 21:43:40 +0700

    Feat: Add delegate, add numberPad, hideKeyboard, move up keyboard, edit mode

---
+++ Reeach/Page/SetupPage/Component/AddBudget/GoalAllocation/Model/Protocol/GoalAllocationModalDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  GoalAllocationModalDelegate.swift
+//  Reeach
+//
+//  Created by Balqis on 17/11/22.
+//
+
+import UIKit
+
+protocol GoalAllocationModalViewCOntrollerDelegate: AnyObject {
+    func viewDismissed()
+}
--- Reeach/Page/SetupPage/Component/AddBudget/GoalAllocation/View/GoalAllocationModalView.swift
+++ Reeach/Page/SetupPage/Component/AddBudget/GoalAllocation/View/GoalAllocationModalView.swift
@@ -12,7 +12,6 @@ class GoalAllocationModalView: UIView {
     
     let iconWithoutEdit = IconWithoutEditView()
     
-    
     let goalName = TextField(frame: .zero, title: "Judul", style: .template)
     
     let dueDate = DatePicker(frame: .zero, title: "Deadline")
@@ -43,28 +42,32 @@ class GoalAllocationModalView: UIView {
         return label
     }()
     
-    let saveButton = Button(style: .rounded, foreground: .primary, background: .tangerineYellow, title: "Simpan")
-
-
+    let deleteButton = {
+        let btn = Button(style: .rounded, foreground: .destructive, background: .darkSlateGrey, title: "Hapus Kebutuhan")
+        btn.backgroundColor = .red6
+        btn.setTitleColor(UIColor.ghostWhite, for: .normal)
+        return btn
+    }()
+    
+    let saveButton = {
+        let btn = Button(style: .rounded, foreground: .destructive, background: .darkSlateGrey, title: "Simpan")
+        btn.backgroundColor = .black4
+        btn.setTitleColor(UIColor.black7, for: .normal)
+        return btn
+    }()
+    
     
     //scrollView
     lazy var scrollView: UIScrollView = {
-            let scrollView = UIScrollView()
-            return scrollView
-        }()
+        let scrollView = UIScrollView()
+        return scrollView
+    }()
     
     
     func configureStackView() {
         self.backgroundColor = .ghostWhite
         
-        let vstack = UIStackView(arrangedSubviews: [iconWithoutEdit,
-                                                    goalName,
-                                                    dueDate,
-                                                    target,
-                                                    inflationButton,
-                                                    monthlySaving,
-                                                    budgetRemain,
-                                                   saveButton]
+        let vstack = UIStackView(arrangedSubviews: [iconWithoutEdit, goalName, dueDate, target, inflationButton, monthlySaving, budgetRemain]
         )
         
         vstack.frame = self.bounds
@@ -73,23 +76,75 @@ class GoalAllocationModalView: UIView {
         vstack.spacing = 16
         vstack.setCustomSpacing(12, after: target)
         vstack.setCustomSpacing(12, after: monthlySaving)
-
+        
+        let buttonStackView = UIStackView(arrangedSubviews: [saveButton, deleteButton])
+        buttonStackView.frame = self.bounds
+        buttonStackView.axis = .vertical
+        buttonStackView.distribution = .fillEqually
+        buttonStackView.spacing = 12
+        
+        let stackView = UIStackView(arrangedSubviews: [vstack, buttonStackView])
+        stackView.frame = self.bounds
+        stackView.axis = .vertical
+        stackView.distribution = .equalSpacing
         
         //autolayout
         self.addSubview(scrollView)
-        scrollView.addSubview(vstack)
+        scrollView.addSubview(stackView)
         
-        scrollView.anchor(top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, width: UIScreen.main.bounds.width)
+        scrollView.anchor(top: safeAreaLayoutGuide.topAnchor, left: safeAreaLayoutGuide.leftAnchor, bottom: safeAreaLayoutGuide.bottomAnchor, right: safeAreaLayoutGuide.rightAnchor)
         
-        vstack.anchor(top: scrollView.topAnchor, left: scrollView.leftAnchor, bottom: scrollView.bottomAnchor, right: scrollView.rightAnchor, paddingLeft: 20, paddingRight: 20)
+        stackView.anchor(top: scrollView.topAnchor, left: scrollView.leftAnchor, bottom: scrollView.bottomAnchor, right: scrollView.rightAnchor, paddingLeft: 20, paddingBottom: 20, paddingRight: 20)
         
         NSLayoutConstraint.activate([
-            vstack.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -40)
+            stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -40),
+            stackView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
         ])
         
         iconWithoutEdit.setUp()
+        iconWithoutEdit.anchor(top: vstack.topAnchor, paddingTop: 20)
         iconWithoutEdit.heightAnchor.constraint(equalTo: iconWithoutEdit.iconLabel.heightAnchor).isActive = true
-                
+        
     }
+    
+    /*
+     func configureStackView() {
+     self.backgroundColor = .ghostWhite
+     
+     let vstack = UIStackView(arrangedSubviews: [iconWithoutEdit,
+     goalName,
+     dueDate,
+     target,
+     inflationButton,
+     monthlySaving,
+     budgetRemain,
+     saveButton]
+     )
+     
+     vstack.frame = self.bounds
+     vstack.axis = .vertical
+     vstack.distribution = .fill
+     vstack.spacing = 16
+     vstack.setCustomSpacing(12, after: target)
+     vstack.setCustomSpacing(12, after: monthlySaving)
+     
+     
+     //autolayout
+     self.addSubview(scrollView)
+     scrollView.addSubview(vstack)
+     
+     scrollView.anchor(top: self.topAnchor, left: self.leftAnchor, bottom: self.bottomAnchor, right: self.rightAnchor, width: UIScreen.main.bounds.width)
+     
+     vstack.anchor(top: scrollView.topAnchor, left: scrollView.leftAnchor, bottom: scrollView.bottomAnchor, right: scrollView.rightAnchor, paddingLeft: 20, paddingRight: 20)
+     
+     NSLayoutConstraint.activate([
+     vstack.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -40)
+     ])
+     
+     iconWithoutEdit.setUp()
+     iconWithoutEdit.heightAnchor.constraint(equalTo: iconWithoutEdit.iconLabel.heightAnchor).isActive = true
+     
+     }
+     */
 }
 
