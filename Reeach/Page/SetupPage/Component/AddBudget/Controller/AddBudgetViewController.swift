f1599da1a900f144683bae98965c18acfa8a399a
James Christian Wira <jamesch888@gmail.com>
2022-11-18 01:41:09 +0700

    Fix: Business logic for BudgetList
    Detail changes:
    Feat: AddBudget -> AddBudgetViewController & AddBudget (View)
    Feat: Add placeholder case to disable button -> Logic to be implemented
    Fix: BudgetView now can show added budget(s)

---
+++ Reeach/Page/SetupPage/Component/AddBudget/Controller/AddBudgetViewController.swift
@@ -0,0 +1,45 @@
+//
+//  AddBudgetViewController.swift
+//  Reeach
+//
+//  Created by James Christian Wira on 18/11/22.
+//
+
+import UIKit
+
+class AddBudgetViewController: UIViewController {
+
+    let addBudgetView = AddBudget()
+    
+    weak var delegate: SetupDelegate?
+    
+    var goalBudgets: [Budget] = []
+    var needBudgets: [Budget] = []
+    var wantBudgets: [Budget] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.view = addBudgetView
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {        
+        goalBudgets = DatabaseHelper().getBudgets(on: Date(), type: "Goal")
+        needBudgets = DatabaseHelper().getBudgets(on: Date(), type: "Need")
+        wantBudgets = DatabaseHelper().getBudgets(on: Date(), type: "Want")
+        
+        addBudgetView.goalBudgets = self.goalBudgets
+        addBudgetView.needBudgets = self.needBudgets
+        addBudgetView.wantBudgets = self.wantBudgets
+        
+        addBudgetView.delegate = delegate
+        
+        addBudgetView.goalStack.removeFromSuperview()
+        addBudgetView.needStack.removeFromSuperview()
+        addBudgetView.wantStack.removeFromSuperview()
+        
+        addBudgetView.setupView()
+    }
+
+}
