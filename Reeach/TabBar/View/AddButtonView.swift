786310c8b8116a4708f94f452ff2015d0e0ad864
James Christian Wira <jamesch888@gmail.com>
2022-12-08 22:53:06 +0700

    Fix: image and title layout inside button

--- Reeach/TabBar/View/AddButtonView.swift
+++ Reeach/TabBar/View/AddButtonView.swift
@@ -30,8 +30,8 @@ class AddButtonView: UIView {
         btn.titleLabel?.font = .bodyBold
         btn.titleLabel?.textAlignment = .left
         btn.setTitleColor(.black13, for: .normal)
-        btn.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 100)
-        btn.titleEdgeInsets = UIEdgeInsets(top: 0, left: 32, bottom: 0, right: -32)
+        btn.titleEdgeInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+        btn.contentHorizontalAlignment = .leading
         return btn
     }()
     
@@ -42,27 +42,22 @@ class AddButtonView: UIView {
         btn.titleLabel?.font = .bodyBold
         btn.titleLabel?.textAlignment = .left
         btn.setTitleColor(.black13, for: .normal)
-        btn.contentEdgeInsets = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 80)
-        btn.titleEdgeInsets = UIEdgeInsets(top: 0, left: 32, bottom: 0, right: -32)
+        btn.titleEdgeInsets = UIEdgeInsets(top: 0, left: 20, bottom: 0, right: 0)
+        btn.contentHorizontalAlignment = .leading
         return btn
     }()
     
     func configureAutoLayout() {
 
-        let navStackView = UIStackView(arrangedSubviews: [titleLabel, closeButton]
-        )
+        let navStackView = UIStackView(arrangedSubviews: [titleLabel, closeButton])
         navStackView.frame = self.bounds
         navStackView.axis = .horizontal
         navStackView.distribution = .equalSpacing
         
-        let btnStackView = UIStackView(arrangedSubviews: [newGoal, newTransaction]
-        )
-        btnStackView.frame = self.bounds
+        let btnStackView = UIStackView(arrangedSubviews: [newGoal, newTransaction])
         btnStackView.axis = .vertical
         btnStackView.distribution = .fillEqually
         btnStackView.spacing = 20
-        btnStackView.alignment = .leading
-        
         
         backgroundColor = .ghostWhite
         
@@ -70,6 +65,5 @@ class AddButtonView: UIView {
         navStackView.anchor(top: topAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 24, paddingLeft: 20, paddingRight: 20)
         addSubview(btnStackView)
         btnStackView.anchor(top: navStackView.bottomAnchor, left: leftAnchor, right: rightAnchor, paddingTop: 30, paddingLeft: 20, paddingRight: 20)
-
     }
 }
