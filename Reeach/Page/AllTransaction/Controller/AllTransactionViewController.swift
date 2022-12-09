2b51dafc078a64f32486c78551245b960f8f41a9
James Christian Wira <jamesch888@gmail.com>
2022-12-09 21:25:43 +0700

    Fix: Dismiss keyboard & empty search
    Additional changes:
    - Code clean up

--- Reeach/Page/AllTransaction/Controller/AllTransactionViewController.swift
+++ Reeach/Page/AllTransaction/Controller/AllTransactionViewController.swift
@@ -25,8 +25,6 @@ class AllTransactionViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-        
         self.view = allTransactionView
     }
     
@@ -38,7 +36,14 @@ class AllTransactionViewController: UIViewController {
         allTransactionView.transactionList.delegate = self
         allTransactionView.transactionList.dataSource = self
         allTransactionView.delegate = self
+        allTransactionView.searchBar.delegate = self
         allTransactionView.setupView()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        
+        view.dismissKeyboard()
     }
     
     func configureData() {
@@ -62,6 +67,9 @@ class AllTransactionViewController: UIViewController {
         })
         
         separatedTransactions = getSeparatedTransactions(transactions: searchedTransaction)
+        
+        allTransactionView.emptyView.isHidden = !separatedTransactions.isEmpty
+        allTransactionView.transactionList.isHidden = separatedTransactions.isEmpty
         
         updateView()
     }
@@ -87,6 +95,7 @@ class AllTransactionViewController: UIViewController {
     
     func updateView() {
         allTransactionView.transactions = separatedTransactions
+        allTransactionView.setupView()
         allTransactionView.transactionList.reloadData()
     }
     
@@ -113,7 +122,6 @@ class AllTransactionViewController: UIViewController {
     }
     
     func openFilterSheet() {
-        
         let navigationController = UINavigationController()
         navigationController.navigationItem.largeTitleDisplayMode = .never
         navigationController.navigationBar.setValue(true, forKey: "hidesShadow")
@@ -128,93 +136,3 @@ class AllTransactionViewController: UIViewController {
     }
 }
 
-extension AllTransactionViewController: TransactionDelegate {
-    func openSheet() {
-        openFilterSheet()
-    }
-    
-    func search(searchText: String) {
-        self.searchText = searchText
-        
-        if searchText.isEmpty {
-            configureData()
-        } else {
-            configureSearchData()
-        }
-    }
-    
-    func dismiss() {
-        dismissDelegate?.viewDismissed()
-        self.dismiss(animated: true)
-    }
-    
-    func filterTransaction(startMonth: Date?, endMonth: Date?, type: String?, budgetCategory: Category?) {
-        configureFilteredData(startMonth: startMonth, endMonth: endMonth, type: type, categoryBudget: budgetCategory)
-    }
-    
-    func openTransactionModal(transaction: Transaction?) {
-        print("Nothing to do here")
-    }
-}
-
-
-extension AllTransactionViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
-        print(#function)
-    }
-    
-    func numberOfSections(in collectionView: UICollectionView) -> Int {
-        sortedKeys.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        self.separatedTransactions[self.sortedKeys[section]]!.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = allTransactionView.transactionList.dequeueReusableCell(withReuseIdentifier: TransactionItemViewCell.identifier, for: indexPath) as! TransactionItemViewCell
-        
-        cell.setupData(transaction: self.separatedTransactions[sortedKeys[indexPath.section]]![indexPath.item])
-        cell.setupView()
-        
-        return cell
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        switch kind {
-            case UICollectionView.elementKindSectionHeader:
-                let cell = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: HeaderGoalDetailCollectionReusableView.reuseIdentifier, for: indexPath) as! HeaderGoalDetailCollectionReusableView
-                
-                let dateString = DateFormatHelper.getDDddMMyyy(from: sortedKeys[indexPath.section])
-                cell.titleLabel.text = dateString
-                
-                return cell
-            default:
-                print("Nothing here")
-            
-        }
-        
-        return UICollectionReusableView()
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize{
-        return CGSize(width: collectionView.frame.width, height: 60)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return CGSize(width: collectionView.frame.width, height: 50)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, heightForHeaderInSection section: Int) -> CGFloat {
-        return 20
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, heightForFooterInSection section: Int) -> CGFloat {
-        return 20
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 12
-    }
-}
